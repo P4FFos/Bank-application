96be9cf2176b372a818181744b5baf25edeef355
Julia McCall <mccall@student.chalmers.se>
2023-11-23 10:55:07 +0100

    updated User class and created Bank class

---
+++ Bank.java
@@ -0,0 +1,47 @@
+import src.customers.Account;
+import src.customers.Customer;
+import src.employees.BankEmployee;
+import src.utils.ContactCard;
+
+import java.util.HashMap;
+
+public class Bank {
+    private ContactCard contactInfo;
+    private HashMap<String, Customer> customers;
+    private HashMap<String, BankEmployee> employees;
+
+    public Bank(ContactCard contactInfo) {
+        this.contactInfo = contactInfo;
+        this.customers = new HashMap<String, Customer>();
+        this.employees = new HashMap<String, BankEmployee>();
+    }
+
+    public ContactCard getBankInfo() {
+
+    }
+
+    public void addCustomer(Customer Customer) {
+
+    }
+
+    public void removeCustomer(String employeeID, String customerID) {
+
+    }
+
+    public Customer getCustomer(String userId) {
+
+    }
+
+    public void createEmployee(BankEmployee employee) {
+
+    }
+
+    public void removeEmployee(String employeeId) {
+
+    }
+
+    public Account getAccountById(String accountId) {
+
+    }
+
+}
