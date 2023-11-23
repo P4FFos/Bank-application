782e11930474789d467b8171bf2b02f96d949877
Danylo Baranov <danilbaranov58@gmail.com>
2023-11-23 11:34:45 +0100

    Danylo - Customer Abstract class and sub Corporate/Private customers classes

--- src/customers/CustomerPrivate.java
+++ src/customers/CustomerPrivate.java
@@ -1,5 +1,22 @@
 package src.customers;
 
-public class CustomerPrivate extends Customer{
+import src.utils.ContactCard;
 
+public class CustomerPrivate extends Customer {
+    CustomerPrivate(String userId, String password, ContactCard contactCardCorporate) {
+        super(userId, password, contactCardCorporate);
+    }
+
+    public boolean isEqual(CustomerPrivate anotherObject) {
+        boolean isEqual = false;
+        if (anotherObject == this) {
+            isEqual = true;
+        } else if (anotherObject == null) {
+            isEqual = false;
+        } else {
+            CustomerPrivate anotherCustomer = (CustomerPrivate) anotherObject;
+            boolean sameUserID = this.userId.equals(anotherCustomer.getUserId());
+        }
+        return isEqual;
+    }
 }
