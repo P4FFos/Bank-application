d3cc22c0ed9d278a890b59978c4baf962d9de565
Danylo Baranov <danilbaranov58@gmail.com>
2023-11-23 11:54:49 +0100

    Danylo - Account and Transaction classes

--- src/customers/Account.java
+++ src/customers/Account.java
@@ -1,9 +1,51 @@
 package src.customers;
 
-import src.Bank;
-import src.exceptions.AccountNotFoundException;
-
 import java.util.ArrayList;
-public class Account {
+import java.util.Date;
 
+public class Account {
+    final String accountId;
+    private double balance;
+    private ArrayList<Transaction> transactions;
+
+    Account(String accountId, double balance) {
+        this.accountId = accountId;
+        this.balance = balance;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public ArrayList<Transaction> getHistory() {
+        return transactions;
+    }
+
+    public void deposit(String receiverAccountId, Date date, double amount) {
+        balance += amount;
+        Transaction deposit = new Transaction(receiverAccountId, amount, date);
+        transactions.add(deposit);
+    }
+
+    public void withdraw(Date date, double amount) throws Exception {
+        if (balance >= amount) {
+            balance = balance - amount;
+            Transaction withdraw = new Transaction(amount, date);
+            transactions.add(withdraw);
+        } else {
+            throw new Exception("");
+        }
+    }
+
+    public void outgoingTransaction(String receiverAccountId, double amount, String date) {
+
+    }
+
+    public void incomingTransaction(double amount, String date) {
+
+    }
 }
--- src/customers/Customer.java
+++ src/customers/Customer.java
@@ -1,4 +1,26 @@
 package src.customers;
 
+import src.utils.ContactCard;
+
+import java.util.HashMap;
+
 public abstract class Customer extends User {
+    private HashMap<accountId, Account> accounts;
+
+    Customer(String userId, String password, ContactCard contactCard) {
+        this.accounts = new HashMap<>;
+        userId = User.getUserId();
+        contactCard = User.getContactInfo();
+    }
+
+    public Account getAccount(accountId) {
+        for (Account account : accounts) {
+            if (account.getAccountId().equals(accountId)) {
+                return account;
+            }
+        }
+        return null;
+    }
+
 }
+
