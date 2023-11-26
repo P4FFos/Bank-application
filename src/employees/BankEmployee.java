0c6b81b7dbd5ae02b363534369626ef7b2a52e87
Aliaksei Khval <guskhval@student.gu.se>
2023-11-26 22:26:35 +0100

    Import fixed and methods uncommented for the merge

--- src/employees/BankEmployee.java
+++ src/employees/BankEmployee.java
@@ -1,12 +1,10 @@
 package src.employees;
 
-import src.utils.ContactCard;
-import src.utils.ContactCardPrivate;
+import src.ContactCard;
 import src.customers.User;
 
 public abstract class BankEmployee extends User {
-	public BankEmployee(String userId, String password, ContactCardPrivate contactCard){
-		//TODO: Implement this constructor
-		//super(userId, password, contactCard);
+	public BankEmployee(String userId, String password, ContactCard contactCard){
+		super(userId, password, contactCard);
 	}
 }
--- src/employees/KeyAccountManager.java
+++ src/employees/KeyAccountManager.java
@@ -1,28 +1,30 @@
 package src.employees;
 
-import src.utils.ContactCardCorporate;
-import src.utils.ContactCardPrivate;
+import src.Bank;
+import src.customers.Account;
+import src.customers.CustomerCorporate;
+import src.ContactCardCorporate;
 
 public class KeyAccountManager extends BankEmployee{
-	public KeyAccountManager (String userId, String password, ContactCardPrivate contactCard){
+	public KeyAccountManager (String userId, String password, ContactCardCorporate contactCard){
 		super(userId, password, contactCard);
 	}
 
 	public void createCustomerCorporate(String userId, String password, ContactCardCorporate contactCard){
-		//CustomerCorporate newCustomer = new CustomerCorporate(userId, password, contactCard);
-		//Bank.addCustomer(newCustomer);
+		CustomerCorporate newCustomer = new CustomerCorporate(userId, password, contactCard);
+		Bank.addCustomer(newCustomer);
 	}
 
 	public void removeCustomerCorporate(String userId){
-		//Bank.removeCustomer(userId);
+		Bank.removeCustomer(userId);
 	}
 
 	public void createAccountCorporate(String userId, String accountNumber){
-		//Account newAccount = new Account(accountNumber);
-		//Bank.getCustomer(userId).addAccount(newAccount);
+		Account newAccount = new Account(accountNumber);
+		Bank.getCustomer(userId).addAccount(newAccount);
 	}
 
 	public void removeAccountCorporate(String userId, String accountNumber){
-		//Bank.getCustomer(userId).removeAccount(accountNumber);
+		Bank.getCustomer(userId).removeAccount(accountNumber);
 	}
 }
