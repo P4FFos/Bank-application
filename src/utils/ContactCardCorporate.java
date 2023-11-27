8b6f21a629431b24dabf7fb8fc543631096538fc
kozlov <kozlov@chalmers.student.se>
2023-11-27 13:53:06 +0100

    moved card to utils

---
+++ src/utils/ContactCardCorporate.java
@@ -0,0 +1,28 @@
+package src.utils;
+
+import src.ContactCard;
+
+public class ContactCardCorporate extends ContactCard{
+    private final String orgNumber;
+    private String name;
+
+    public ContactCardCorporate(name, orgNumber, email, phoneNumber, streetAddress, zipCode, city){
+        this.name = name;
+        this.orgNumber = orgNumber;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.streetAddress = streetAddress;
+        this.zipCode = zipCode;
+        this.city = city;
+    }
+    public String getName(){
+        this.name = name;
+    }
+    public void setName(){
+        this.name = newName;
+    }
+    public String getOrgNumber(){
+        return orgNumber;
+    }
+    
+}
---
+++ src/utils/ContactCardPrivate.java
@@ -0,0 +1,31 @@
+package src.utils;
+
+import src.ContactCard;
+
+public class ContactCardPrivate extends ContactCard{
+    private String firstName;
+    private String surname;
+    private final String SSN;
+
+    public ContactCardPrivate(firstName, surname, SSN, email, phoneNumber, streetAddress, zipCode, city){
+        this.firstName = firstName;
+        this.surname = surname;
+        this.SSN = SSN;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.streetAddress = streetAddress;
+        this.zipCode = zipCode;
+        this.city = city;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getSurname(){
+        return surname;
+    }
+    public String getSSN(){
+        return SSN;
+    }
+}
+
