d459fdcce598a21360ef4e94bbcf0d6f645dd501
kozlov <kozlov@chalmers.student.se>
2023-11-27 15:44:26 +0100

    remove name from everywhere

--- src/utils/ContactCardCorporate.java
+++ src/utils/ContactCardCorporate.java
@@ -2,21 +2,12 @@ package src.utils;
 
 public class ContactCardCorporate extends ContactCard {
     private final String orgNumber;
-    private String name;
+   
 
-    public ContactCardCorporate(String name, String orgNumber, String email, String phoneNumber, String streetAddress, String zipCode, String city) {
-        super(name, email, phonenumber, streetAdress, zipCode, city);
+    public ContactCardCorporate(String orgNumber, String email, String phoneNumber, String streetAddress, int zipCode, String city) {
+        super(email, phoneNumber, streetAddress, zipCode, city);
         this.orgNumber = orgNumber;
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName() {
-        this.name = Newname;
-    }
-
     public String getOrgNumber() {
         return orgNumber;
     }
