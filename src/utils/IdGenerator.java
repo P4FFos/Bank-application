7cc7a4d2d2227833c38e7ab6837196d8006ec6e1
Danylo Baranov <danilbaranov58@gmail.com>
2023-11-28 20:53:46 +0100

    Danylo - start point for IDGenerator for customer/employee

--- src/utils/IdGenerator.java
+++ src/utils/IdGenerator.java
@@ -1,4 +1,21 @@
 package src.utils;
 
+import src.customers.User;
+import src.customers.Customer;
+
+
 public class IdGenerator {
+    //employee should start with E (both bank teller and KAM)
+    //employee ID should consist of 1 letter (E) and 5 numbers
+    //customer ID should consist of 6 numbers
+    //increment by 1 for each new ID
+    //largest ID number - stored in json file (how to format json file?)
+
+    public void customerIDGenerator(String customerID) throws Exception{
+//        if(getUserId().equals(customerID)){
+//            throw new Exception("");
+//        } else {
+//
+//        }
+    }
 }
