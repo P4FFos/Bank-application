dbb894c3e23a07d3bc4dcb456f2bfa4ae1507a8d
kozlov <kozlov@chalmers.student.se>
2023-11-29 09:57:49 +0100

    contact card updates, getters  and constructors

--- src/utils/ContactCardCorporate.java
+++ src/utils/ContactCardCorporate.java
@@ -1,56 +1,26 @@
 package src.utils;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-import src.ContactCard;
 
-=======
->>>>>>> 82561ad3e6f96aabab37c323cfb313fa499b93ca
-public class ContactCardCorporate extends ContactCard{
-=======
 public class ContactCardCorporate extends ContactCard {
->>>>>>> 117530c168f5743824c28c073355aabbea542e17
     private final String orgNumber;
-   
+    private String companyName;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    public ContactCardCorporate(name, orgNumber, email, phoneNumber, streetAddress, zipCode, city){
-=======
-    public ContactCardCorporate(String name,String orgNumber,String  email, String phoneNumber,String streetAddress,String zipCode,String city){
-<<<<<<< HEAD
->>>>>>> 82561ad3e6f96aabab37c323cfb313fa499b93ca
-=======
-       super(name, email, phonenumber, streetAdress,zipCode,city);
->>>>>>> 7bf1c8347cd3a771532ded4e90535b456820970f
-        this.name = name;
-=======
-    public ContactCardCorporate(String orgNumber, String email, String phoneNumber, String streetAddress, int zipCode, String city) {
+    public ContactCardCorporate(String companyName, String orgNumber, String email, String phoneNumber, String streetAddress, int zipCode, String city) {
         super(email, phoneNumber, streetAddress, zipCode, city);
->>>>>>> 117530c168f5743824c28c073355aabbea542e17
         this.orgNumber = orgNumber;
+        this.companyName = companyName;
     }
-<<<<<<< HEAD
-    public String getName(){
-<<<<<<< HEAD
-        this.name = name;
+    public String getCompanyName(){
+        return companyName;
     }
-    public void setName(){
-        this.name = newName;
-    }
-=======
-        return name;
-    }
-    public void setName(){
-        this.name = Newname;
+    public void setCompanyName(){
+        this.companyName = companyName;
     }
     
->>>>>>> 82561ad3e6f96aabab37c323cfb313fa499b93ca
-    public String getOrgNumber(){
-=======
     public String getOrgNumber() {
->>>>>>> 117530c168f5743824c28c073355aabbea542e17
         return orgNumber;
     }
 
 }
+    
+
--- src/utils/ContactCardPrivate.java
+++ src/utils/ContactCardPrivate.java
@@ -1,36 +1,22 @@
 package src.utils;
 
-<<<<<<< HEAD
-import src.ContactCard;
-
-=======
->>>>>>> 82561ad3e6f96aabab37c323cfb313fa499b93ca
 public class ContactCardPrivate extends ContactCard{
     private final String SSN;
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-    public ContactCardPrivate(firstName, surname, SSN, email, phoneNumber, streetAddress, zipCode, city){
-=======
-    public ContactCardPrivate(String firstName, String surname, String SSN,String email,String phoneNumber, String streetAddress, String zipCode, String city){
-<<<<<<< HEAD
->>>>>>> 82561ad3e6f96aabab37c323cfb313fa499b93ca
-=======
-        super(name, email, phoneNumber, streetAddress, zipCode, city);
->>>>>>> 7bf1c8347cd3a771532ded4e90535b456820970f
-        this.firstName = firstName;
-        this.surname = surname;
-=======
-    public ContactCardPrivate(String SSN,String email,String phoneNumber, String streetAddress, int zipCode, String city){
+    private String firstName;
+    private String lastName;
+    public ContactCardPrivate(String firstName, String lastName, String SSN,String email,String phoneNumber, String streetAddress, int zipCode, String city){
         super(email, phoneNumber, streetAddress, zipCode, city);
->>>>>>> 117530c168f5743824c28c073355aabbea542e17
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.SSN = SSN;
     }
     public String getSSN(){
         return SSN;
     }
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> 82561ad3e6f96aabab37c323cfb313fa499b93ca
