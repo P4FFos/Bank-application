88adb3d1bea86380ab5a42ed54cb4174bfdbd16f
Danylo Baranov <danilbaranov58@gmail.com>
2023-12-01 13:06:04 +0100

    Danylo - IDGenerator for customer

--- src/utils/IdGenerator.java
+++ src/utils/IdGenerator.java
@@ -6,11 +6,25 @@ public class IdGenerator {
     //customer ID should consist of 6 numbers
     //increment by 1 for each new ID
     //largest ID number - stored in json file (how to format json file?)
+    private final static int MIN_CUSTOMER_ID_LENGTH = 1;
+    private final static int MAX_CUSTOMER_ID_LENGTH = 6;
+    private final static int MAX_CUSTOMER_ID = 999999;
 
-    public void customerIDGenerator() throws Exception {
-        String customerId = String.format("%06d", lastFoundId);
-        customerId = lastFoundId + 1;
-
-        // create lastFoundID method in bank
+    public static String generateCustomerID(String lastEmployeeID) throws Exception {
+        boolean isEmployeeIDValid = false;
+        boolean isEmployeeIDLengthValid = lastEmployeeID.length() >= MIN_CUSTOMER_ID_LENGTH || lastEmployeeID.length() <= MAX_CUSTOMER_ID_LENGTH;
+        boolean isEmployeeIDNumbersValid = lastEmployeeID.matches("[0-9]+");
+        isEmployeeIDValid = isEmployeeIDLengthValid && isEmployeeIDNumbersValid;
+        if (!isEmployeeIDValid) {
+            throw new Exception("Invalid employee ID");
+        } else {
+            int nextID = Integer.parseInt(lastEmployeeID);
+            nextID++;
+            if (nextID > MAX_CUSTOMER_ID) {
+                throw new Exception("Employee ID out of range");
+            } else {
+                return Integer.toString(nextID);
+            }
+        }
     }
 }
