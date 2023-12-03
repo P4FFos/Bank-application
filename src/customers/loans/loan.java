173394357597bb0978d77a64b7fc3752b1af8d4a
Aliaksei Khval <guskhval@student.gu.se>
2023-12-03 21:08:24 +0100

    loan main attributes, getters, constructor

--- src/customers/loans/loan.java
+++ src/customers/loans/loan.java
@@ -1,5 +1,30 @@
 package src.customers.loans;
 
-public class loan {
+import java.util.Calendar;
+
+public class Loan {
+	private Calendar initialLoanDate;
+	private double loanAmount;
+	private double interestRate;
+
+	public Loan(Calendar initialLoanDate, double loanAmount, double interestRate) {
+		this.initialLoanDate = initialLoanDate;
+		this.loanAmount = loanAmount;
+		this.interestRate = interestRate;
+	}
+
+	public Calendar getInitialLoanDate() {
+		return this.initialLoanDate;
+	}
+
+	public double getLoanAmount() {
+		return this.loanAmount;
+	}
+
+	public double getInterestRate() {
+		return this.interestRate;
+	}
+
+
 
 }
