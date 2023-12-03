ebd2869e2d3ec08f1c8d193c6378f70e3a1b48cd
Tanya Nordh <tanyan@net.chalmers.se>
2023-12-03 15:23:56 +0100

    comments in UIMain.java

--- ui/src/main/java/com/piggybank/app/ui/UIMain.java
+++ ui/src/main/java/com/piggybank/app/ui/UIMain.java
@@ -5,20 +5,26 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import src.Bank;
+import src.*;
+import src.utils.ContactCard;
 import src.utils.ContactCardCorporate;
 
-
 public class UIMain extends Application {
-
+    private Bank bank;
     @Override
     public void start(Stage stage) {
         //LATER
         //Initialize Bank
         //Read mock data from json
-
         ContactCardCorporate contactCardBank = new ContactCardCorporate("123412", "email@email.com", "0734567811", "Street No1", 90210, "Beverly Hills");
-        Bank bank = new Bank(contactCardBank);
+        bank = new Bank(contactCardBank);
+        // src/Bank: createEmployee() must be amended before I can use it here. Would be better to name it "addEmployee" and have it create
+        // and add an employee to the HashMap of employees. Should not take in an employee as a parameter. Where would such an employee be
+        // created?? /Tanya
+
+        //String userId, BankEmployee employee:
+        //String userId, String password, ContactCard contactCard)
+        //contactCard: String email, String phoneNumber, String streetAddress, int zipCode, String city
 
         try {
             Parent root = FXMLLoader.load(getClass().getResource("StartScene.fxml"));
@@ -29,6 +35,10 @@ public class UIMain extends Application {
         } catch(Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public Bank getBank(){
+        return bank;
     }
 
     public static void main(String[] args) {
