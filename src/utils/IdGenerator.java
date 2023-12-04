5edc4b9f02b5b6c3dfcda603b1e8d93e3780e3a1
Aliaksei Khval <guskhval@student.gu.se>
2023-12-04 15:35:57 +0100

    Employee Id generator exceptions removed

--- src/utils/IdGenerator.java
+++ src/utils/IdGenerator.java
@@ -5,8 +5,6 @@ public class IdGenerator {
     private final static int MAX_CUSTOMER_ID_LENGTH = 6;
     private final static int MAX_CUSTOMER_ID = 999999;
 
-    private final static int MIN_EMPLOYEE_ID_LENGTH = 2;
-    private final static int MAX_EMPLOYEE_ID_LENGTH = 6;
     private final static int MAX_EMPLOYEE_ID = 99999;
 
     public static String generateCustomerID(String lastCustomerID) throws Exception {
@@ -22,13 +20,6 @@ public class IdGenerator {
     }
 
     public static String generateEmployeeID(String lastEmployeeID) throws Exception {
-        boolean isEmployeeIDValid = false;
-        boolean isEmployeeIDStartValid = lastEmployeeID.startsWith("e");
-        boolean isEmployeeIDLengthValid = lastEmployeeID.length() >= MIN_EMPLOYEE_ID_LENGTH || lastEmployeeID.length() <= MAX_EMPLOYEE_ID_LENGTH;
-        boolean isEmployeeIDNumbersValid = lastEmployeeID.substring(1).matches("[0-9]+");
-        isEmployeeIDValid = isEmployeeIDStartValid && isEmployeeIDLengthValid && isEmployeeIDNumbersValid;
-        if (!isEmployeeIDValid) // if the last employee ID is not in the correct format
-            throw new Exception("Invalid employee ID");
         int nextID = Integer.parseInt(lastEmployeeID.substring(1));
         nextID++;
         if (nextID > MAX_EMPLOYEE_ID) // if the next employee ID is out of range
