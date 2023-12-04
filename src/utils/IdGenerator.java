2f89914a0a150ea05002a88fd18507f2b396fc04
Danylo Baranov <danilbaranov58@gmail.com>
2023-12-04 15:37:11 +0100

    Danylo - deleted exceptions from the generateCustomerID method

--- src/utils/IdGenerator.java
+++ src/utils/IdGenerator.java
@@ -1,22 +1,19 @@
 package src.utils;
 
 public class IdGenerator {
-    private final static int MIN_CUSTOMER_ID_LENGTH = 1;
-    private final static int MAX_CUSTOMER_ID_LENGTH = 6;
     private final static int MAX_CUSTOMER_ID = 999999;
 
     private final static int MAX_EMPLOYEE_ID = 99999;
 
     public static String generateCustomerID(String lastCustomerID) throws Exception {
-        boolean isCustomerIDValid = false;
-        boolean isCustomerIDLengthValid = lastCustomerID.length() >= MIN_CUSTOMER_ID_LENGTH || lastCustomerID.length() <= MAX_CUSTOMER_ID_LENGTH;
-        boolean isCustomerIDNumbersValid = lastCustomerID.matches("[0-9]+");
-        isCustomerIDValid = isCustomerIDLengthValid && isCustomerIDNumbersValid;
-
         int nextID = Integer.parseInt(lastCustomerID);
         nextID++;
 
-        return Integer.toString(nextID);
+        if (nextID > MAX_CUSTOMER_ID) {
+            throw new Exception("Employee ID out of range");
+        } else {
+            return Integer.toString(nextID);
+        }
     }
 
     public static String generateEmployeeID(String lastEmployeeID) throws Exception {
