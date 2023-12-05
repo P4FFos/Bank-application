cc2fae4dc16c4a1bfc30acf73dca0dd2bd99087e
Danylo Baranov <danylob@student.chalmers.se>
2023-12-05 12:13:58 +0100

    Danylo - changes done in the getLoanAmountWithInterest method in cooperation with Aliaksei

--- src/customers/loans/Loan.java
+++ src/customers/loans/Loan.java
@@ -1,8 +1,6 @@
 package src.customers.loans;
 
 import java.util.Calendar;
-import java.util.Date;
-
 import src.utils.TruncationUtil;
 
 public class Loan {
@@ -33,19 +31,24 @@ public class Loan {
     }
 
     // getLoanAmountWithInterest method which:
+    // checks monthDifference between current and initial months
+    // checks if currentMonth date smaller than initialMonth
+    // counts totalLoanAmountWithInterest
+    // returns truncated amount
     public double getLoanAmountWithInterest() {
         Calendar currentMonth = Calendar.getInstance();
         Calendar initialMonth = getInitialLoanDate();
 
-        int monthDifference = (currentMonth.get(Calendar.YEAR) - initialMonth.get(Calendar.YEAR)) * 12 + currentMonth.get(Calendar.MONTH) - initialMonth.get(Calendar.MONTH);
+        int monthDifference = (currentMonth.get(Calendar.YEAR) - initialMonth.get(Calendar.YEAR)) * 12 + currentMonth.get(Calendar.MONTH) - initialMonth.get(Calendar.MONTH) + 1;
+        if (currentMonth.get(Calendar.DAY_OF_MONTH) < initialMonth.get(Calendar.DAY_OF_MONTH)) {
+            monthDifference--;
+        }
 
         double totalLoanAmountWithInterest = getLoanAmount();
-
         for (long i = 1; i < monthDifference; i++) {
             double interestLoanAmount = totalLoanAmountWithInterest * (getInterestRate() / 100);
             totalLoanAmountWithInterest += interestLoanAmount;
         }
-
         return TruncationUtil.truncate(totalLoanAmountWithInterest);
     }
 }
