7994fc518b5880b1dcce73435868dc4c1a867b31
Tanya Nordh <tanyan@net.chalmers.se>
2023-12-08 15:32:08 +0100

    scene and controller for customerOverview

--- ui/src/main/java/com/piggybank/app/ui/EmpCustomerOverviewController.java
+++ ui/src/main/java/com/piggybank/app/ui/EmpCustomerOverviewController.java
@@ -2,21 +2,22 @@ package com.piggybank.app.ui;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class EmployeeCustomerOverviewController extends EmployeeMainController implements Initializable {
-    @FXML
-    private AnchorPane customerCard;
+public class EmpCustomerOverviewController extends EmpMainController implements Initializable {
     @FXML
     private Label customerIdLabel;
+    @FXML
+    private Label customerSSNLabel;
     @FXML
     private Label customerNameLabel;
     @FXML
@@ -26,11 +27,27 @@ public class EmployeeCustomerOverviewController extends EmployeeMainController i
     @FXML
     private Button selectAccountButton;
     @FXML
+    private Button addAccountButton;
+    @FXML
+    private Button makeTransactionButton;
+    @FXML
+    private CheckBox inTransactionCheckBox;
+    @FXML
+    private CheckBox outTransactionCheckBox;
+    @FXML
     private ListView<String> accountsListView; //will be list of current customer's accounts
     @FXML
-    private ListView<String> accountHistoryListView; //will be list of the current account's transactions
+    private ListView<String> transactionsListView; //will be list of the current account's transactions
 
+    //--------------
+    //Placeholders: current customer should be of type Customer when possible
+    private String currentCustomerName;
+    private String currentCustomerSSN;
+    private String currentCustomerId;
+    //______________
+    //Placeholder: will be derived from current customer when possible
     private String[] accounts = {"main_account", "savings", "emergency", "travel"};
+    //--------------
     private String currentAccount = "";
 
     public void initialize(URL arg0, ResourceBundle arg1){
@@ -43,10 +60,26 @@ public class EmployeeCustomerOverviewController extends EmployeeMainController i
             }
         });
     } //Initializes accountsListView with elements in accounts, selection sets currentAccount
-    public void showAccount(){
-        accountIdLabel.setText("0123");
-        accountBalanceLabel.setText("25530.12");
-        accountHistoryListView.getItems().add(currentAccount);
-    } //Just a test to fill the ListView that will show transaction history from selecting an account in accountsListView
+
+    public void displayCurrentCustomer(String id, String name, String ssn){ //change parameter to Customer and modify implementation accordingly
+        customerIdLabel.setText(id);
+        customerSSNLabel.setText(ssn);
+        customerNameLabel.setText(name);
+    }
+
+    //--------------- METHODS CONNECTED TO FXML ELEMENTS -------------------------
+    public void setCurrentAccount(ActionEvent event){
+
+    }
+
+    public void addAccount(ActionEvent event){
+
+    }
+
+    public void makeTransaction(ActionEvent event){
+
+    }
+
+
 
 }
