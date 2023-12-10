f9387826c6852cdb221fc7c88b1ac4fd9f49a845
Tanya Nordh <tanyan@net.chalmers.se>
2023-12-10 21:38:38 +0100

    created instance of Bank + BankTeller for testing purposes

--- ui/src/main/java/com/piggybank/app/ui/UIMain.java
+++ ui/src/main/java/com/piggybank/app/ui/UIMain.java
@@ -5,18 +5,27 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import src.Bank;
+import src.employees.BankEmployee;
+import src.employees.BankTeller;
+import src.utils.ContactCard;
 
 import java.util.HashMap;
 import java.util.Map;
-//import src.Bank;
-//import src.utils.ContactCard;
 
 public class UIMain extends Application {
+    public static Bank bank;
+    public static Bank getBank(){
+        return bank;
+    }
 
     @Override
     public void start(Stage stage) {
-        //ContactCard bankContactInfo = new ContactCard("employee@piggybank.com", "0701234567", "Oink Oink Alley", 35533, "Trufflesville");
-        //Bank bank = new Bank(bankContactInfo);
+        ContactCard bankContactInfo = new ContactCard("employee@piggybank.com", "0701234567", "Oink Oink Alley", 35533, "Trufflesville");
+        bank = new Bank(bankContactInfo);
+        ContactCard employeeContactCard = new ContactCard("moiraine@sedai.com", "0731234567", "White Tower", 77777, "Tar Valon");
+        BankEmployee employee = new BankTeller("e001", "dragon", employeeContactCard);
+        bank.createEmployee("e001", employee);
 
         try {
             Parent root = FXMLLoader.load(getClass().getResource("StartScene.fxml"));
