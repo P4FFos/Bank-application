b030b62b761dfa46dae9ded674a9a11f5307a120
Aliaksei Khval <guskhval@student.gu.se>
2023-12-11 15:06:19 +0100

    new design for Employees

--- src/employees/BankTeller.java
+++
@@ -1,9 +0,0 @@
-package src.employees;
-
-import src.utils.ContactCard;
-
-public class BankTeller extends BankEmployee {
-	public BankTeller (String userId, String password, ContactCard contactCard){
-		super(userId, password, contactCard);
-	}
-}
