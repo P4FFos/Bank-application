28fcaf13c34bab84b689d1641cfdbc02ad770cc5
Marcus Berggren <marbergg@student.chalmers.se>
2023-12-16 20:34:59 +0100

    cleaned up, some more testing

--- ui/src/main/java/com/piggybank/app/backend/BackendMain.java
+++ ui/src/main/java/com/piggybank/app/backend/BackendMain.java
@@ -1,32 +1,24 @@
 package com.piggybank.app.backend;
 
-import java.io.File;
-import java.time.LocalDate;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.piggybank.app.backend.customers.Customer;
 import com.piggybank.app.backend.utils.ContactCard;
+import com.piggybank.app.backend.utils.FileHandler;
 
 public class BackendMain {
-
-    public static Bank jsonExample() throws Exception {
-        ObjectMapper objectMapper = new ObjectMapper();
-        // to enable java.time.LocalDate module in Jackson, this is needed. Used in Transaction class
-        objectMapper.registerModule(new JavaTimeModule());
-        File objJsonfile = new File("ui/src/main/java/com/piggybank/app/backend/bankData.json");
-        return objectMapper.readValue(objJsonfile, Bank.class);
-    }
     public static void main(String[] args) throws Exception {
-        Bank bank = jsonExample();
+
+        String jsonFile = "ui/src/main/java/com/piggybank/app/backend/bankData.json";
+        Bank bank = FileHandler.jsonDeserializer(jsonFile);
 
         // testing the bank
         System.out.println(bank.getCustomerByIdOrSsn("C001").getEmail());
         System.out.println(bank);
         System.out.println(bank.getCustomerByIdOrSsn("C002").getAccounts());
-        System.out.println(bank.getCustomerIdCounter());
         System.out.println(bank.getBankInfo());
         System.out.println(bank.getTransactionHistory("A00001"));
+
+        ContactCard contactCard = new ContactCard("", "", "", "", "");
+        bank.createCustomerPrivate("0801010101", "John", "Doe", "DRAGON1122", contactCard);
+        System.out.println(bank.getCustomerByIdOrSsn("0801010101").toString());
     }
 }
 
