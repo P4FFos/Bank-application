03eca3f7f04c2b0e3f933dbc9e53e7922d63532b
Marcus Berggren <marbergg@student.chalmers.se>
2023-12-16 10:28:00 +0100

    gson no longer required

--- ui/src/main/java/module-info.java
+++ ui/src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module com.piggybank.app.ui {
     requires javafx.controls;
     requires javafx.fxml;
-    requires com.google.gson;
     requires com.fasterxml.jackson.databind;
 
     opens com.piggybank.app.ui to javafx.fxml;
