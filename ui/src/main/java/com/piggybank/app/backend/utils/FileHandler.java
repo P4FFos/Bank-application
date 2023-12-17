04745b0fa6f376812d64247df30d7344510a6423
Marcus Berggren <marbergg@student.chalmers.se>
2023-12-17 11:58:21 +0100

    added jsonSerializer

--- ui/src/main/java/com/piggybank/app/backend/utils/FileHandler.java
+++ ui/src/main/java/com/piggybank/app/backend/utils/FileHandler.java
@@ -1,6 +1,7 @@
 package com.piggybank.app.backend.utils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.piggybank.app.backend.Bank;
 
@@ -14,8 +15,20 @@ public class FileHandler {
         // to enable java.time.LocalDate module in Jackson, this is needed. LocalDate is used in Transaction class
         objectMapper.registerModule(new JavaTimeModule());
 
-        File objJsonfile = new File(filePath);
-        return objectMapper.readValue(objJsonfile, Bank.class);
+        File jsonfile = new File(filePath);
+        return objectMapper.readValue(jsonfile, Bank.class);
     }
-    public static void jsonSerializer() {}
+    public static void jsonSerializer(String filePath, Bank bank) throws IOException {
+        // jackson class, provides functionality when reading and writing json files
+        ObjectMapper objectMapper = new ObjectMapper();
+        // to enable java.time.LocalDate module in Jackson, this is needed. LocalDate is used in Transaction class
+        objectMapper.registerModule(new JavaTimeModule());
+
+        // Create an ObjectWriter with the default pretty printer
+        ObjectWriter objectWriter = objectMapper.writerWithDefaultPrettyPrinter();
+
+        File jsonFile = new File(filePath);
+        objectWriter.writeValue(jsonFile, bank);
+
+    }
 }
