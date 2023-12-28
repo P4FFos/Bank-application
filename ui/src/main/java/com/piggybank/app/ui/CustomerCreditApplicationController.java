2edb4e56c62cfde3bf19e12b43218f9c0abb2d89
Tanya Nordh <tanyan@net.chalmers.se>
2023-12-28 17:09:18 +0100

    cleaned up CustomerCreditsOverviewController, implemented initializable, added methods connected to buttons. Implemented initializable in CustomerCreditApplicationController.

--- ui/src/main/java/com/piggybank/app/ui/CustomerCreditApplicationController.java
+++ ui/src/main/java/com/piggybank/app/ui/CustomerCreditApplicationController.java
@@ -1,6 +1,9 @@
 package com.piggybank.app.ui;
 
+import com.piggybank.app.backend.customers.CustomerCorporate;
+import com.piggybank.app.backend.customers.CustomerPrivate;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
@@ -10,7 +13,10 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
-public class CustomerCreditApplicationController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CustomerCreditApplicationController extends CustomerStartController implements Initializable {
 
     @FXML
     private CheckBox agreeCheckBox;
@@ -155,5 +161,25 @@ public class CustomerCreditApplicationController {
 
     @FXML
     private Separator verticalSeparator;
+    @FXML
+    private Label infoActualUserIdLabel;
 
+
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        showCurrentCustomer();
+    }
+
+    @Override
+    public void showCurrentCustomer(){
+        super.showCurrentCustomer();
+        if (currentCustomer instanceof CustomerPrivate) {
+            CustomerPrivate privateCustomer = (CustomerPrivate) currentCustomer;
+            infoActualUserIdLabel.setText(privateCustomer.getUserId());
+            System.out.println("Customer Accounts Overview Page. Logged in as: " + privateCustomer.getFullName());
+        } else {
+            CustomerCorporate corporateCustomer = (CustomerCorporate) currentCustomer;
+            infoActualUserIdLabel.setText(corporateCustomer.getUserId());
+            System.out.println("Customer Accounts Overview Page. Logged in as: " + corporateCustomer.getCompanyName());
+        }
+    }
 }
