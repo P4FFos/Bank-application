f5a63b5c9586779acd77fabd4075a1da225e23fb
kozlov <kozlov@chalmers.student.se>
2024-01-04 15:22:40 +0100

    Added Comments for Contact card

--- ui/src/main/java/com/piggybank/app/backend/utils/ContactCard.java
+++ ui/src/main/java/com/piggybank/app/backend/utils/ContactCard.java
@@ -10,7 +10,7 @@ public class ContactCard {
     // Bare constructor used by Jackson-Databind for Json deserializing
     public ContactCard() {
     }
-
+    //Contact card constructor
     public ContactCard(String email, String phoneNumber, String streetAddress, String zipCode, String city) {
         this.email = email;
         this.phoneNumber = phoneNumber;
@@ -18,11 +18,23 @@ public class ContactCard {
         this.zipCode = zipCode;
         this.city = city;
     }
-
+    //"Getters" methods
     public String getEmail() {
-        return email;
+        return email;    
     }
-
+     public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+     public String getZipCode() {
+        return zipCode;
+    }
+     public String getCity() {
+        return city;
+    }
+    //"Setters" methods
     public void setEmail(String email) throws Exception {
         boolean isAddressValid = email.matches("^(.+)@(\\S+)$"); // regex to match email. Checks if @ is present
         if(isAddressValid) {
@@ -30,41 +42,20 @@ public class ContactCard {
         } else {
             throw new Exception("Email address is not valid.");
         }
-
     }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
-
-    public String getStreetAddress() {
-        return streetAddress;
-    }
-
     public void setStreetAddress(String streetAddress) {
         this.streetAddress = streetAddress;
     }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
     public void setZipCode(String newZip) {
         this.zipCode = newZip;
     }
-
-    public String getCity() {
-        return city;
-    }
-
     public void setCity(String city) {
         this.city = city;
     }
-    
+    //ToString method for ContactCard
     public String toString() {
         return String.format("email: %s, phoneNumber: %s, streetAddress: %s, zipCode: %s, city: %s", email, phoneNumber, streetAddress, zipCode, city);
     }
